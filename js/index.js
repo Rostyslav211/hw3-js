32086ce87d1fae3eded64b8304ad5098ff83d993
Rostyslav Holota <rostislavgolota03@gmail.com>
2024-10-21 17:57:18 +0200

    hw3-js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//    let = result;
+//    result = 5+5+'5';
+//    console.log(result);
+
+   const emeil = 'dfede@gmail.com'
+   console.log(emeil .includes ('@') );
+   console.log(emeil .includes ('emeil') );
+
+   const fullName = 'Viktor'
+   const messege = 'My name is ' + fullName + '.';
+   console.log(messege);
+
+let userName = "Олександра";
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
